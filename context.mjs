46f789dfed63969f1cef9ff3f4634768ee96e93c
Atsushi Oka <oka.ats@gmail.com>
2024-06-05 18:10:32 +0900

    add comments as a documentation of tBC Application System

--- context.mjs
+++ context.mjs
@@ -1,7 +1,11 @@
 
+// Load a module to read `.settings` file.
 import { readSettings        } from  'asynchronous-context/settings' ;
+
+// Load Context Factory Loader which is a module from async-context-rpc package.
 import { loadContextFactory  } from  'asynchronous-context-rpc/context-factory-loader.mjs' ;
 
+// Define a context factory; then, export it.
 export function load_context_factory() {
   const settings = readSettings();
   const {
@@ -11,4 +15,5 @@ export function load_context_factory() {
 
   return loadContextFactory( context_factory, purge_require_cache );
 }
+
 
